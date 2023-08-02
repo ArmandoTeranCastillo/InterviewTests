cd2cd8659655b8381b6cec73dbd03f7e80a7ebda
Armando Terán Castillo <desarrollo1GV@outlook.com>
2023-08-02 16:03:48 -0600

    añadir proyecto

---
+++ InterviewTests/Common/FirstTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InterviewTests.Common;
+
+public abstract class FirstTest
+{
+    public static string ReverseString(string s)
+    {
+        var charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+    
+    //Check palindrome
+    public static string CheckPalindrome(string s)
+    {
+        int i;
+        s = s.ToLower();
+        var normalArray = s.ToCharArray();
+        var reverseArray = s.ToCharArray(); 
+        Array.Reverse(reverseArray);
+        for(i = 0; i < reverseArray.Length; i++)
+        {
+            if (normalArray[i] != reverseArray[i])
+                return "No es palindromo";
+        }
+        return "Es un palindromo";
+    }
+    //Reverse phrase
+    public static string ReversePhrase(string s)
+    {
+        var array = Regex.Split(s, "( )");
+        Array.Reverse(array);
+        return string.Concat(array);
+    }
+
+    public static string ReverseEachString(string s)
+    {
+        var array = Regex.Split(s, "( )");
+        var newArray = new List<string>();
+        foreach (var element in array)
+        {
+            var chararray = element.ToCharArray();
+            Array.Reverse(chararray);
+            newArray.Add(new string (chararray));
+        }
+        return string.Concat(newArray);
+    }
+}
---
+++ InterviewTests/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.RegularExpressions;
+using InterviewTests.Common;
+
+namespace InterviewTests;
+
+public abstract partial class InterviewTests
+{
+    public static void Main()
+    {
+        Console.WriteLine(FirstTest.ReverseEachString("Hola a mis amigos"));
+    }
+    //Reverse String
+   
+}
